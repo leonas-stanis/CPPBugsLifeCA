4819094aaa6751b891e350e7d82465664afcd126
RaivisK2510 <raiviskrums11@gmail.com>
2026-05-05 14:03:10 +0100

    Add displayAllBugs method to Board class

--- Board.cpp
+++ Board.cpp
@@ -65,4 +65,29 @@ void Board::loadFromFile(const string& filename) {
     file.close();
     cout << "Loaded " << bugs.size() << " bugs from file '" << filename << "'.\n";
 }
+void Board::displayAllBugs() const {
+    if (bugs.empty()) {
+        cout << "No bugs on the board. Load a file first.\n";
+        return;
+    }
 
+    cout << "\n--- All Bugs ---\n";
+    for (const Bug* bug : bugs) {
+        // id, type, position, health, direction, (hopLength for hoppers), status
+        cout << bug->getId() << " "
+             << bug->getType() << " ("
+             << bug->getPosition().first << ","
+             << bug->getPosition().second << ") "
+             << bug->getHealth() << " "
+             << bug->getDirectionStr() << " ";
+
+        // Show hopLength for Hopper bugs
+        const Hopper* hopper = dynamic_cast<const Hopper*>(bug);
+        if (hopper) {
+            cout << hopper->getHopLength() << " ";
+        }
+
+        cout << (bug->isAlive() ? "Alive" : "Dead") << "\n";
+    }
+    cout << "----------------\n";
+}
